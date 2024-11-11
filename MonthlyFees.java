520628ac5e391e7709f4b2d8259c4346bebba9b1
Johdie03 <solimanj.college@gmail.com>
2024-11-11 00:16:11 +0000

    commit trial

--- MonthlyFees.java
+++ MonthlyFees.java
@@ -3,7 +3,28 @@ public class MonthlyFees {
     private double fees;
     private double minAccBalance;
     
+    public MonthlyFees(double fees, double minAccBalance) {
+        this.fees = fees;
+        this.minAccBalance = minAccBalance;
+    }
 
+    //setters
+    public void setFees(double fees) {
+        this.fees = fees;
+    }
+
+    public void setMinAccBalance(double minAccBalance) {
+        this.minAccBalance = minAccBalance;
+    }
+
+    //getters
+    public double getFees() {
+        return fees;
+    }
+
+    public double minAccBalance() {
+        return minAccBalance;
+    }
 
 }
 
