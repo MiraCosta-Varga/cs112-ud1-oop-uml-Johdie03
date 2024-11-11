04fe24db64550086d912fb3c55ecb1c8c6397043
Johdie03 <solimanj.college@gmail.com>
2024-11-11 00:16:11 +0000

    commiting

--- MonthlyFees.java
+++ MonthlyFees.java
@@ -3,6 +3,19 @@ public class MonthlyFees {
     private double fees;
     private double minAccBalance;
     
+    public MonthlyFees(double fees, double minAccBalance) {
+        this.fees = fees;
+        this.minAccBalance = minAccBalance;
+    }
+
+    //setters
+    public void setFees(double fees) {
+        this.fees = fees;
+    }
+
+    public void setMinAccBalance(double minAccBalance) {
+        this.minAccBalance = minAccBalance;
+    }
 
 
 }
